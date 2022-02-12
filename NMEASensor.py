a4878daeb6b6c193ece638096242124ce0f814e9
John ORaw <john.oraw@iotech.ie>
2022-02-12 18:42:39 +0000

    First commit

--- NMEASensor.py
+++ NMEASensor.py
@@ -1,5 +1,6 @@
 import serial
 import sys
+from nmea.utilities import udp_sender
 
 # Utilities used for file handling and logging
 from utilities.file import log_file_name as log_file_name
@@ -9,14 +10,18 @@ nmea_log_file_name = './logfiles/' + log_file_name('.nmea')
 # Open the file for append
 nmea_output_file = open(nmea_log_file_name, 'a', newline='')
 
-print('***** NMEA Sensor *****')
+# Set UDP multicast information
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5001
+
+print('***** NMEA Moving Base Sensor *****')
 print('Accepts NMEA from a serial port:')
 print('1. Extracts information and logs raw NMEA')
-print('2. Outputs to an IP address/port for other applications to use.')
+print('2. Outputs to a multicast address 224.1.1.1:5001 for other applications to use.')
 
 try:
-    with serial.Serial("COM14") as serial_port:
-        serial_port.baudrate = 38400
+    with serial.Serial("/dev/ttyAMA2") as serial_port:
+        serial_port.baudrate = 115200
         serial_port.bytesize = serial.EIGHTBITS
         serial_port.parity = serial.PARITY_NONE
         serial_port.stopbits = serial.STOPBITS_ONE
@@ -28,27 +33,6 @@ try:
             if len(byte1) < 1:
                 break
 
-            # Check for UBX header = xB5 and X62, Unicode = µb
-            # Minimal UBX code, just to read the bytes
-            if byte1 == b"\xb5":
-                byte2 = serial_port.read(1)
-                if len(byte2) < 1:
-                    break
-                if byte2 == b"\x62":
-                    # Get the UBX class
-                    byte3 = serial_port.read(1)
-                    # Get the UBX message
-                    byte4 = serial_port.read(1)
-                    # Get the UBX payload length
-                    byte5and6 = serial_port.read(2)
-                    # Calculate the length of the payload
-                    length_of_payload = int.from_bytes(byte5and6, "little", signed=False)
-                    # Read the buffer for the payload length
-                    ubx_payload = serial_port.read(length_of_payload)
-                    # Last two bytes are 2*CRC, save them for later use
-                    ubx_crc_a = serial_port.read(1)
-                    ubx_crc_b = serial_port.read(1)
-
             # Check for NMEA0183, leading with a $ symbol
             elif byte1 == b"\x24":
                 nmea_full_bytes = serial_port.readline()
@@ -56,6 +40,7 @@ try:
                 # Check for corrupted lines
                 if nmea_full_string.isascii():
                     nmea_output_file.writelines(nmea_full_string)
+                    udp_sender(MCAST_GRP, MCAST_PORT, nmea_full_bytes)
                     # Force OS to write each line, not to buffer
                     nmea_output_file.flush()
                     print(f'NMEA: Received {nmea_full_string.strip()}')
